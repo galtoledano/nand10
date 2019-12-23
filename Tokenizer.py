e198ac212927a535ee4f72e04964bf5f71ac2b56
gal <gal.toledano@mail.huji.ac.il>
2019-12-23 13:10:10 +0200

    almost finished tokonizing

--- Tokenizer.py
+++ Tokenizer.py
@@ -9,8 +9,8 @@ class Tokenizer:
     def __init__(self, file):
         self.__file = []
         self.__index = 0
-        self.__current_token = self.advance()
-        self.__current_token_type = self.token_type()
+        self.__current_token = None
+        self.__current_token_type = None
         self.__read_file(file)
 
     def __remove_invalid_syntax(self, line):
@@ -27,20 +27,55 @@ class Tokenizer:
         while line:
             current_line = self.__remove_invalid_syntax(line)
             if current_line != "":
-                self.__file.append(current_line)
+                # add spaces between symbols
+                for char in current_line:
+                    if char in self.symbols:
+                        current_line = current_line.replace(char, " " + char + " ")
+                self.__file += current_line.split()
             line = file.readline()
         file.close()
+        self.find_string()
+
+    def find_string(self):
+        final_file = []
+        is_word = False
+        temp = ""
+        for i in range(len(self.__file)):
+            x = self.__file[i]
+            if is_word:
+                temp += self.__file[i] + " "
+                if self.__file[i].endswith('"'):
+                    is_word = False
+                    final_file.append(temp)
+            else:
+                if self.__file[i].startswith('"'):
+                    is_word = True
+                    temp = self.__file[i] + " "
+                else:
+                    final_file.append(self.__file[i])
+        self.__file = final_file
+        print(self.__file)
 
     def has_more_tokens(self):
-        if self.__index != len(self.__file):
+        if self.__index != len(self.__file) - 1:
             return True
+        if len(self.__current_token) == 1 and self.__current_token == "}": #todo check that it not hidding bags
+            return False
+        return True
 
     def advance(self):
-        # gets the next token from the input
-        return "as33"
+        check = self.__file[self.__index]
+        if check in self.keyWords or check in self.symbols:
+            self.__current_token = check
+        elif check[0] in self.symbols:
+            temp1 = check[0]
+            temp2 = check[1:]
+
+        self.__index += 1
+        self.token_type()
 
     def token_type(self):
-        # returns the token's type. dict ?
+        # returns the token's type.
         if self.__current_token in self.keyWords:
             return "keyWord"
         elif self.__current_token in self.symbols:
@@ -52,10 +87,8 @@ class Tokenizer:
         else:
             return "identifier"
 
-
     def keyword(self):
-        # return keyword
-        pass
+        return str(self.__current_token)
 
     def symbol(self):
         # return char
@@ -70,3 +103,31 @@ class Tokenizer:
 
     def string_val(self):
         return str(self.__current_token)
+
+
+    def find_string2(self):
+        lst = self.__file
+        is_word = False
+        temp = ""
+        counter = 0
+        i = 0
+        while i < len(lst):
+            x = lst[i]
+            if is_word:
+                temp += lst[i] + " "
+                counter += 1
+                if lst[i].endswith('"'):
+                    is_word = False
+                    i = i + counter
+                    self.__file = self.__file[:index] + [temp] + self.__file[i:]
+                    counter = 0
+                else:
+                    i += 1
+
+            else:
+                if lst[i].startswith('"'):
+                    is_word = True
+                    index = i
+                    temp = lst[i] + " "
+                i += 1
+        print(self.__file)
---
+++ project10.py
@@ -0,0 +1,5 @@
+import Tokenizer
+file = "test"
+
+t = Tokenizer.Tokenizer(file)
+a = t.string_val()
