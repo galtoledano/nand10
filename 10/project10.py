f8ea50b2186d1e7525b1c4e0dcd425ed675f21bf
gal <gal.toledano@mail.huji.ac.il>
2019-12-25 20:32:27 +0200

    ex 11: done symbol table done vm writer at the middle of the compilation engine

---
+++ 10/project10.py
@@ -0,0 +1,13 @@
+import Tokenizer
+import CompilationEngine
+file = "test2.jack"
+
+# t = Tokenizer.Tokenizer(file)
+# f = CompilationEngine.CompilationEngine(file, "out")
+# a = t.string_val()
+
+print('"'.endswith('"'))
+print('"'.startswith('"'))
+print("//*ggg".find("/*"))
+x = "//*"
+print(x[1+2:])
