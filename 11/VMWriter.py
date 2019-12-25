3adc7f139537c23311178489a062965266bc9605
gal <gal.toledano@mail.huji.ac.il>
2019-12-25 19:24:52 +0200

    ex 11: done symbol table done vm writer

--- 11/VMWriter.py
+++ 11/VMWriter.py
@@ -1,33 +1,33 @@
 class VMWriter:
-    def __init__(self):
-        pass
+    def __init__(self, output_path):
+        self.__out = open(output_path, "w")
 
-    def write_push(self, segment):
-        pass
+    def write_push(self, segment, index):
+        self.__out.write("push {0} {1}".format(segment, index))
 
-    def write_pop(self, segment):
-        pass
+    def write_pop(self, segment, index):
+        self.__out.write("pop {0} {1}".format(segment, index))
 
     def write_arithmetic(self, command):
-        pass
+        self.__out.write(command)  # todo lowercase ?
 
     def write_label(self, label):
-        pass
+        self.__out.write("({0})".format(label))
 
     def write_goto(self, label):
-        pass
+        self.__out.write("goto {0}".format(label))
 
     def write_if(self, label):
-        pass
+        self.__out.write("if-goto {0}".format(label))
 
     def write_call(self, name, num_of_args):
-        pass
+        self.__out.write("call {0} {1}".format(name, num_of_args))
 
     def write_function(self, name, num_of_locals):
-        pass
+        self.__out.write("function {0} {1}".format(name, num_of_locals))
 
     def write_return(self):
-        pass
+        self.__out.write("return")
 
     def close(self):
-        pass
+        self.__out.close()
